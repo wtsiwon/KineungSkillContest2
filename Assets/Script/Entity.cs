6d44b56b79658becc18fc3ae81814b06820a048d
wtsiwon <siwon4727@gmail.com>
2023-02-24 20:47:21 +0900

    .,

---
+++ Assets/Script/Entity.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [SerializeField]
+    protected float maxHp;
+    [SerializeField]
+    protected float hp;
+    public float Hp
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            if (hp <= 0) OnDie();
+            else if (hp >= maxHp) hp = maxHp;
+            else hp = value;
+        }
+    }
+
+    [SerializeField]
+    protected float bulletSpd;
+    [SerializeField]
+    protected float moveSpd;
+    [SerializeField]
+    protected float atkSpd;
+
+    [SerializeField]
+    protected GameObject bullet;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected virtual void OnDie()
+    {
+
+    }
+}
